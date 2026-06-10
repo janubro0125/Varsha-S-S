e6a7dde636cbcc70999b09ef452793b3ae7ded8c
janubro0125 <janubro.0125@gmail.com>
2026-06-10 11:28:21 +0530

    Add files via upload

---
+++ Lab 1 TASK VARSHA S S.py
@@ -0,0 +1,114 @@
+"""
+name="Varsha"
+print(name)
+print(type(name))
+age=22
+print(age)
+print(type(age))
+help("keywords")#this is used to show keywords
+
+print("Data types")
+a=10
+b=20
+print(type(a))
+print(type(b))
+#22+44j complex numbers
+c=22+44j
+print(type(c))
+
+print("Arithmetic operators")
+print(a+b)
+print(a-b)
+print(b-a)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+"""
+"""
+print("Assignment operators")
+a=20
+a+=2
+print(a)
+b=10
+b-=3
+print(b)
+c=5
+c*=4
+print(c)
+d=10
+d//=5
+print(d)
+e=10
+e**=3
+print(e)
+"""
+"""
+print("Realtional operator")
+a=10
+b=20
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+"""
+"""
+print("Logical operator")
+a=10
+b=20
+print(a<b and a!=b)
+print(a>b and a==b)
+print(a<b and a==b)
+print(a<b or a==b)
+print(a<b or a!=b)
+print(not(a<b and a!=b))
+print(not(a<b or a!=b))
+"""
+"""
+print("Bitwise operator")
+a=5
+b=10
+print(a&b)
+print(a^b)
+print(a|b)
+print(a>>3)
+print(a<<3)
+print(b>>2)
+print(b<<2)
+"""
+"""
+print("1.Swapping")
+a=10
+b=3
+a=a^b
+b=a^b
+a=a^b
+print("a=",a)
+print("b=",b)
+"""
+"""
+print("2.Last value")
+a=54321
+print(a%100)
+"""
+"""
+print("3.Middle value")
+a=54321
+b=a//100
+print(b%10)
+"""
+"""
+print("4.Square")
+a=98765
+b=a//100
+c=b%10
+d=c**2
+print("Square of",c,"=",d)
+"""
+"""
+print("5.Last digit")
+a=1103
+print(a%10)
+"""
