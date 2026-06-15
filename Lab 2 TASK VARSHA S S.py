45b4abdd70393c22d26b0a28c5e89bd5578ee8d4
janubro0125 <janubro.0125@gmail.com>
2026-06-15 11:23:53 +0530

    Add files via upload

---
+++ Lab 2 TASK VARSHA S S.py
@@ -0,0 +1,98 @@
+"""
+print("1. Sqaure or Rectangle")
+a=int(input("Enter length:"))
+b=int(input("Enter breadth:"))
+if a==b:
+    print("Square")
+else:
+    print("Rectangle")
+"""
+"""
+print("2.Check square")
+a=int(input("Enter a number:"))
+b=a*a
+if b>=50:
+    print("Square is above 50")
+else:
+    print("Square is below 50")
+"""
+"""
+print("3.Check difference")
+a=int(input("Enter a number a:"))
+b=int(input("Enter a number b:"))
+c=a-b
+if c==0:
+    print("The difference is 0")
+else:
+    print("The difference is not 0")
+"""
+"""
+print("4.Computer science marks")
+mark=int(input("Enter your marks:"))
+if mark>=50:
+    print("Pass")
+else:
+    print("Fail")
+"""
+"""
+print("5.Check Divisible by 10")
+num=int(input("Enter a number:"))
+if num%10==0:
+    print("The number is divisible by 10")
+else:
+    print("The number is not divisible by 10")
+"""
+"""
+print("6.Biggest digit")
+num=int(input("Enter a two digit number:"))
+digit1=num//10
+digit2=num%10
+if digit1>digit2:
+    print("Biggest digit:",digit1)
+else:
+    print("Biggest digit:",digit2)
+"""
+"""
+print("7.Can go out and play or not")
+num=int(input("Enter a number:"))
+if num==1:
+    print("You can go out and play")
+else:
+    print("You cannot go out and play")
+"""
+"""
+print("8.Exam is easy or difficult")
+num=int(input("Enter a number:"))
+if num==1:
+    print("Exam will be easy")
+else:
+    print("Exam is difficult")
+"""
+"""
+print("9.Basic Calculator")
+num1=int(input("Enter num1:"))
+num2=int(input("Enter num2:"))
+Operator=input("Enter an mathematical operation:")
+if Operator=="+":
+    print("+=",num1+num2)
+elif Operator=="-":
+    print("-=",num1-num2)
+elif Operator=="*":
+    print("*=",num1*num2)
+elif Operator=="/":
+    print("/=",num1/num2)
+"""
+"""
+print("13.Buzz number")
+num=int(input("Weight of parcel:"))
+if num%7==0 or num%10==7:
+    print("Buzz number")
+else:
+    print("Not Buzz number")
+"""
+print("14.Automorphic number")
+num1=int(input("Enter number:"))
+num2=num1*num1
+firstdigit=num1
+lastdigit=num2%10
+if lastdiigit==
